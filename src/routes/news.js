cec1bc5a0254bb3dc4112a5559dc861a9652a23d
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-23 15:09:33 +0100

    update

--- src/routes/news.js
+++ src/routes/news.js
@@ -53,4 +53,30 @@ newsRouter.get('/:id', async(req, res) => {
 })
 
 
+newsRouter.post('', async(req, res) => {
+    let articleID = req.params.id
+
+    try {
+        const newsApi = await axios.get(`https://raddy.dev/wp-json/wp/v2/posts/${articleID}`);
+        res.render('newsSingle', { article: newsApi.data })
+
+    } catch (error) {
+
+        if(error.response) {
+            res.render('newsSingle', { article: null })
+            console.log(error.response.data)
+            console.log(error.response.headers)
+            console.log(error.response.status)            
+        } else if(error.request) {
+            res.render('newsSingle', { article: null  })
+            console.log(error.request);
+        } else {
+            res.render('newsSingle', { article: null})
+            console.error("Error", error.message)
+        }
+
+    }
+})
+
+
 module.exports = newsRouter
